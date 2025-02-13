83f5ea72f85bd178c1675c8c3e057c123fa9beb1
shubham <shubhambhawar2343@gmail.com>
2025-02-13 22:06:01 +0530

    started contact vault

---
+++ src/main/java/com/scv/ContactVault/ContactVaultApplication.java
@@ -0,0 +1,13 @@
+package com.scv.ContactVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scv/ContactVault/ContactVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scv.ContactVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
